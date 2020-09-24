69d43b9857ca05efb52cfa7bb9cabaac5d512615
ytyaru <yttry0@gmail.com>
2020-09-24 19:25:47 +0900

    引数の不具合を修正した。サブクエリ省略できるが実行速度が遅くなったのでコメントアウトした。

--- src/run.sh
+++ src/run.sh
@@ -9,9 +9,21 @@ Run() {
 	cd "$HERE"
 	DB_PATH='Names.db'
 	NUM_DEFAULT=40
+	NUM=${1:-$NUM_DEFAULT}
 	IsInt() { test 0 -eq $1 > /dev/null 2>&1 || expr $1 + 0 > /dev/null 2>&1; }
-	IsInt "$1" && NUM=$1 || NUM=$NUM_DEFAULT
+	IsInt "$NUM" || NUM=$NUM_DEFAULT
 	[ $NUM -lt 1 ] && NUM=$NUM_DEFAULT
+	# サブクエリ省略版。遅い。1.369s
+#	SQL="$(cat <<-EOS
+#		select ROW_NUMBER() over (order by L.Yomi,F.Yomi) as SN,L.Yomi,F.Yomi,L.Kaki,F.Kaki,F.Sex from 
+#		  (select ROW_NUMBER() over (order by random(), Id) as R,Yomi,Kaki from LastNames limit @NUM) as L 
+#		  inner join  
+#		  (select ROW_NUMBER() over (order by random(), Id) as R,Yomi,Kaki,Sex from FirstNames limit @NUM) as F
+#		  on L.R=F.R
+#		  order by L.Yomi,F.Yomi;
+#		EOS
+#	)"
+	# サブクエリ非省略版。速い。0.580s
 	SQL="$(cat <<-EOS
 		select ROW_NUMBER() over (order by L.Yomi,F.Yomi) as SN,L.Yomi,F.Yomi,L.Kaki,F.Kaki,F.Sex from 
 		  (select ROW_NUMBER() over (order by random(), Id) as R,Yomi,Kaki from LastNames where Id in (select Id from LastNames order by random() limit @NUM)) as L 
